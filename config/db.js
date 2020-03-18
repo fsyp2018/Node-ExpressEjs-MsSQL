7890ebfc0ca75e995f54f52d1744a6de71b512b4
fsyp2018 <45746305+fsyp2018@users.noreply.github.com>
2020-03-18 16:41:20 +0800

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const db = {
+    'user': 'sa',
+    'password': 'lee1217',
+    'server': 'LING-PC',
+    'database': 'demo'
+}
+
+module.exports = db;
---
+++ routes/index.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../config/db')
+var sql = require('mssql')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  sql.connect(db, function (err) {
+    if (err) {
+      console.log(err)
+    }
+    var requset = new sql.Request();
+    requset.query("SELECT TOP 10 [id],[userid],[pwd],[username],[email] FROM [demo].[dbo].[UserList]", function (err, result) {
+      if (err) {
+        console.log(err)
+        res.send(err)
+      }
+      sql.close();
+      // res.send(result)
+      res.render('index', { data: result.recordset });
+    })
+  })
+});
+
+router.get('/edit/:id', function (req, res, next) {
+  sql.connect(db, function (err) {
+    if (err) {
+      console.log(err)
+    }
+    var requset = new sql.Request();
+    requset.input('id', sql.Int, req.params.id)
+    requset.query("SELECT * FROM [demo].[dbo].[UserList] where id = @id", function (err, result) {
+      if (err) {
+        console.log(err)
+        res.send(err)
+      }
+      sql.close();
+      res.render('edit', { data: result.recordset[0] });
+    })
+  })
+})
+
+router.post('/update', function (req, res, next) {
+
+  sql.connect(db, function (err) {
+    if (err)
+      console.log(err);
+
+    var request = new sql.Request();
+    request.input('id', sql.Int, req.body.id)
+      .input('username', sql.NVarChar(50), req.body.username)
+      .input('pwd', sql.NVarChar(50), req.body.pwd)
+      .input('email', sql.NVarChar(50), req.body.email)
+      .query('update UserList set username=@username,pwd=@pwd,email=@email where id=@id', function (err, result) {
+
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        sql.close();
+        res.redirect('/');
+      });
+  });
+});
+
+router.get('/add', function (req, res, next) {
+  res.render('add', {
+    route: 'add',
+  });
+});
+
+router.post('/add', function (req, res, next) {
+
+  sql.connect(db, function (err) {
+    if (err)
+      console.log(err);
+
+    var request = new sql.Request();
+    request.input('userid', sql.NVarChar(50), req.body.userid)
+      .input('pwd', sql.NVarChar(50), req.body.pwd)
+      .input('username', sql.NVarChar(50), req.body.username)
+      .input('email', sql.NVarChar(50), req.body.email)
+      .query('insert into UserList (userid, pwd, username, email) values (@userid, @pwd, @username, @email)', function (err, result) {
+
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        sql.close();
+        res.redirect('/');
+      });
+  });
+});
+
+router.get('/delete/:id', function (req, res, next) {  
+  sql.connect(db, function (err) {
+    if (err)
+      console.log(err);
+
+    var request = new sql.Request();
+    request.input('id', sql.Int, req.params.id)
+      .query('delete from UserList where id=@id', function (err, result) {
+
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        sql.close();
+        res.redirect('/');
+      });
+  });
+});
+
+module.exports = router;
